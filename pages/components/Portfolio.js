c55e9113fbbaddfa45bc78a9e0646d100a9f9488
BRAZILLIER Johan <j.brazillier@gmail.com>
2022-01-09 12:09:01 +0100

    portfolio

--- pages/components/Portfolio.js
+++ pages/components/Portfolio.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 export default function Portfolio() {
+    const projects = {
+        1: { title: 'test&' },
+        2: { title: 'test1' },
+        3: { title: 'test2' }
+    }
+    const loop = []
+
+    for (let i in projects) {
+        loop.push(
+            <p>{projects[i].title}</p>
+        )
+    }
+
     return (
-        <h1 id='projects' className='big-title2'>PORTFOLIO</h1>
+        <div>
+            <h1 id='projects' className='big-title2'>PORTFOLIO</h1>
+            <p>{loop}</p>
+        </div>
     )
 }
